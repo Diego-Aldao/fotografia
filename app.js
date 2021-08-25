7db9173b386f7e262307ca30898c2a8bdf690158
unknown <shizusenpai97@gmail.com>
2021-08-25 15:40:10 -0300

    añadida la navegacion y el scrolleo horizontal al header de la pagina principal

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".header-principal", {
   speed: 1500,
   parallax: true,
   loop: true,
+  mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -11,3 +12,33 @@ var swiper = new Swiper(".header-principal", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//API INTESERCTION OBSERVER
+const crearObservador = (animacion, nuevaClase) => {
+  const secciones = document.querySelectorAll(animacion);
+
+  const options = {
+    root: null, //es el default, todo el viewport
+    threshold: 0.26, // 0 dispara en el momento que el elemento entra al observador, 1 dispara cuando todo el elemento ya esta dentro del observador
+  };
+
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } //si no esta en la pantalla, termina la funcion
+      entry.target.classList.add(nuevaClase);
+
+      observer.unobserve(entry.target); //para que las animaciones solo se ejecuten la primera vez que se entra en la seccion.
+    });
+  }, options);
+  secciones.forEach((section) => {
+    observer.observe(section); //al usar querySelectorAll tengo un array con las secciones, y lo que quiero lograr es observar uno por uno
+  });
+};
+crearObservador(".animacion-uno", "final-uno");
+crearObservador(".animacion-dos", "final-dos");
+crearObservador(".animacion-tres", "final-tres");
+crearObservador(".animacion-cuatro", "final-cuatro");
+
+//SCROLL HORIZONTAL
