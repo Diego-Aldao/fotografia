5a9ba04040541194589e5918df5e72a1fe647dbd
unknown <shizusenpai97@gmail.com>
2021-09-09 15:43:36 -0300

    mejorado el botton flotante up y puesto un condicional en el codigo swiper para que solo se ejecute cuando este en la pagina inicio

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
+window.onload = () => {
+  //SWIPER
+  if (document.body.id === "inicio") {
+    var swiper = new Swiper(".header-principal", {
+      speed: 1500,
+      parallax: true,
+      loop: true,
+      mousewheel: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    var swiper = new Swiper(".swiper", {
+      centeredSlides: true,
+      spaceBetween: 100,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+};
+//API INTESERCTION OBSERVER
+const crearObservador = (animacion, nuevaClase) => {
+  const secciones = document.querySelectorAll(animacion);
+
+  const options = {
+    root: null, //es el default, todo el viewport
+    threshold: 0.26, // 0 dispara en el momento que el elemento entra al observador, 1 dispara cuando todo el elemento ya esta dentro del observador
+  };
+
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } //si no esta en la pantalla, termina la funcion
+      entry.target.classList.add(nuevaClase);
+
+      observer.unobserve(entry.target); //para que las animaciones solo se ejecuten la primera vez que se entra en la seccion.
+    });
+  }, options);
+  secciones.forEach((section) => {
+    observer.observe(section); //al usar querySelectorAll tengo un array con las secciones, y lo que quiero lograr es observar uno por uno
+  });
+};
+crearObservador(".animacion-uno", "final-uno");
+crearObservador(".animacion-dos", "final-dos");
+crearObservador(".animacion-tres", "final-tres");
+crearObservador(".animacion-cuatro", "final-cuatro");
+
 //NAVEGACION MOVIL
 const btnMenu = document.querySelector(".flotante-menu");
 const btnCerrar = document.querySelector(".flotante-menu-movil");
@@ -49,7 +101,6 @@ linkTrabajosDesk.addEventListener("mouseover", () => {
     margin: "-35px 0px 0px",
     duration: 0.3,
   });
-  console.log("ad");
 });
 menuDesk.addEventListener("mouseleave", () => {
   gsap.to(menuDesk, {
@@ -62,51 +113,25 @@ menuDesk.addEventListener("mouseleave", () => {
   });
 });
 
-//API INTESERCTION OBSERVER
-const crearObservador = (animacion, nuevaClase) => {
-  const secciones = document.querySelectorAll(animacion);
-
-  const options = {
-    root: null, //es el default, todo el viewport
-    threshold: 0.26, // 0 dispara en el momento que el elemento entra al observador, 1 dispara cuando todo el elemento ya esta dentro del observador
-  };
-
-  const observer = new IntersectionObserver(function (entries, observer) {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        return;
-      } //si no esta en la pantalla, termina la funcion
-      entry.target.classList.add(nuevaClase);
-
-      observer.unobserve(entry.target); //para que las animaciones solo se ejecuten la primera vez que se entra en la seccion.
+//BTN FLOTANTE UP
+const btnFlotanteUp = document.querySelector(".flotante-up");
+const scrollearBarraSuperior = () => {
+  if (
+    document.body.scrollTop > 350 ||
+    document.documentElement.scrollTop > 350
+  ) {
+    gsap.to(btnFlotanteUp, {
+      display: "flex",
+      opacity: 1,
     });
-  }, options);
-  secciones.forEach((section) => {
-    observer.observe(section); //al usar querySelectorAll tengo un array con las secciones, y lo que quiero lograr es observar uno por uno
-  });
+  } else {
+    gsap.to(btnFlotanteUp, {
+      display: "none",
+      opacity: 0,
+    });
+  }
 };
-crearObservador(".animacion-uno", "final-uno");
-crearObservador(".animacion-dos", "final-dos");
-crearObservador(".animacion-tres", "final-tres");
-crearObservador(".animacion-cuatro", "final-cuatro");
 
-//SWIPER
-var swiper = new Swiper(".header-principal", {
-  speed: 1500,
-  parallax: true,
-  loop: true,
-  mousewheel: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-var swiper = new Swiper(".swiper", {
-  centeredSlides: true,
-  spaceBetween: 100,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+window.onscroll = () => {
+  scrollearBarraSuperior();
+};
